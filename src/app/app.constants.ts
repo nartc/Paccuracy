fd55ac2f8caba9e9a75873526c282a1bb23d1971
Chau <ctch5@mail.umsl.edu>
2018-02-19 08:49:15 -0600

    Attempt to fix build

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,2 @@
-export const API_ENDPOINT = "http://api.paccurate.io";
+export const API_ENDPOINT = "https://api.paccurate.io";
 export const API_KEY = "J6R4eORMcZ2rhrIA0ybLRkDYekWJOTfb_Ix_qOgw4X4";
